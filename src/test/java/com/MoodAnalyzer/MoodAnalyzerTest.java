649cd5a47bbeb8a4365762624bf03ce87a8e7c00
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-10-09 22:52:50 +0530

    Functionallity to check for sad mood else Return False

--- src/test/java/com/MoodAnalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/MoodAnalyzer/MoodAnalyzerTest.java
@@ -9,13 +9,18 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class MoodAnalyzerTest{
-
-   
+//Test case for sad mood
     @Test
-    public void given_Happy_Mood_should_Return_Happy()
+    public void given_Happy_Mood_should_Return_Sad()
     {
     	MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain();
         String mood = moodAnalyzer.analyseMood("I am In A SAD Mood");
         assertEquals("SAD", mood); 
+    }
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        assertEquals("HAPPY", mood);
     }
 }
